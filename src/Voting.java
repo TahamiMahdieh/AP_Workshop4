3e3af60bd7ff636eb2a1d1c09142b863f3f98067
unknown <MohammadReza Shafeei>
2024-04-12 20:30:39 +0330

    added vote methods to voting

--- src/Voting.java
+++ src/Voting.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
+import java.util.*;
 
 public class Voting {
     private int type;
@@ -28,8 +25,53 @@ public class Voting {
             availableChoices.add(s);
         return availableChoices;
     }
-    private void creatChoice(String choice){
+    public void createChoice(String choice){
         this.choices.put(choice, null);
+    }
+    public void vote(Person voter,ArrayList<String> voter_choices){
+        if (isAnonymous){
+            return;
+        }
+        Vote newVote = new Vote(voter,null);
+        //iteration over field "choices" keyset
+        for (String i: choices.keySet()){
+            //iteration over voter's choices
+            for (String j:voter_choices){
+                //checking if the choices we have in the field equals to the voters choice and adding a new vote to the values of the hashmap(it was set to null in the beginning)
+                if (i.equals(j)){
+                    choices.get(i).add(newVote);
+                }
+            }
+        }
+        //adding the person to the field "voters" if he wasn't already in the arraylist
+        if (!voters.contains(voter)){
+            voters.add(voter);
+        }
+
+    }
+    public void vote(Person person){
+        if (!isAnonymous){
+            return;
+        }
+        Vote newVote = new Vote(person,null);
+        Random random = new Random();
+        // a random number for random voting
+        int randomNum = random.nextInt(choices.size());
+        int counter = 0;
+        for(String i : choices.keySet()){
+            // adding the person to the hashmap based on the random number
+            counter++;
+            if (counter<randomNum){
+                continue;
+            }
+            else {
+                choices.get(i).add(newVote);
+                break;
+            }
+        }
+        if (!voters.contains(person)){
+            voters.add(person);
+        }
     }
 
 }
